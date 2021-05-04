4b3954cbabe09e7a104d610485d19f667836f956
Robert Willian Polli <robert.polli@cnpem.br>
2021-05-04 13:07:13 -0300

    Finishet gamma dac I2C library

--- GammaDAC.py
+++ GammaDAC.py
@@ -1,67 +1,53 @@
-import os
+import board
+import busio
 
 class DAC:
 
-    def __init__(self, i2cAddr, gammaAddr = 15):
+    def __init__(self, gammaAddr = 15):
 
         self.W_addr = int(f'0b001{gammaAddr:04b}0', 2)
         self.R_addr = int(f'0b001{gammaAddr:04b}1',2)
 
-        self.I2C_addr = str(i2cAddr)
+        self.i2c = busio.I2C(board.SCL, board.SDA, 200000)
+
+        self.I2C_addr = [int(i) for i in self.i2c.scan()][0]
         self.voltageREF = 0
 
         self.modeRef = {'00': None, '01': 2.5, '10': 2, '11': 4.1}
 
+    def send_bytes(self, *data):
+        command = []
+        for i in data[0]:
+            command.append(int(i, 2))
+
+        self.i2c.writeto(self.I2C_addr, bytes(command), stop=False)
+
     def power(self, mode, DACs):
-        command = ''
-        for i in [f'0b010000{mode:02b}', f'0b0000{DACs:04b}', '0b00000000']:
-            command += f'{int(i, 2):#04x} '
-        print("i2cset -y -r 2 {} {} 0x00 0x00 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-        os.system("i2cset -y -r 2 {} {} 0x00 0x00 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
+        self.send_bytes([f'010000{mode:02b}', f'0000{DACs:04b}', '00000000'])
 
     def config(self, All_DACs, LD_EN, DACs):
-        command = ''
-        for i in [f'0b0110{All_DACs}00{LD_EN}', f'0b0000{DACs:04b}', '0b00000000']:
-            command += f'{int(i, 2):#04x} '
-        print("i2cset -y -r 2 {} {} 0x00 0x00 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-        os.system("i2cset -y -r 2 {} {} 0x00 0x00 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
+        self.send_bytes([f'0110{All_DACs}00{LD_EN}', f'0000{DACs:04b}', '00000000'])
 
     def ref(self, power, mode):
-        command = ''
         self.voltageREF = self.modeRef[f'{mode:02b}']
-        for i in [f'0b01110{power}{mode:02b}', f'0b00000000', '0b00000000']:
-            command += f'{int(i, 2):#04x} '
-
-        print("i2cset -y -r 2 {} {} 0x00 0x00 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-        os.system("i2cset -y -r 2 {} {} 0x00 0x00 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
+        self.send_bytes([f'01110{power}{mode:02b}', '00000000', '00000000'])
 
     def write_all(self, *value):
-        command = ''
+        data = ['10000010']
         for i in range(len(value[0])):
-            command += f'{value[0][i]:#04x} '
-        print("i2cset -y -r 2 {} {} 0x00 0x00 0x82 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-        os.system("i2cset -y -r 2 {} {} 0x00 0x00 0x82 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
+            data.append(f'{value[0][i]:08b}')
 
-    def writeVolts(self, voltage, all_ch = True):
+        self.send_bytes(data)
+
+    def writeVolts(self, voltage, all_ch = True, ch =0):
         D = int((2**12)*voltage / self.voltageREF)
 
         if voltage > self.voltageREF:
             voltage = self.voltageREF
 
-        command =  f'{int(D//16):#04x} ' + f'{int(D%16) << 4:#04x}'
+        data = ['10000010', f'{int(D//16):08b}', f'{int(D%16) << 4:08b}']
 
-        if all_ch:
-            print("i2cset -y -r 2 {} {} 0x00 0x00 0x82 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-            os.system("i2cset -y -r 2 {} {} 0x00 0x00 0x82 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
+        if not(all_ch):
+            data[0] = f'0011{ch:04b}'
 
-        else:
-            print("i2cset -y -r 2 {} {} 0x00 0x00 0x82 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-            os.system("i2cset -y -r 2 {} {} 0x00 0x00 0x82 {} i".format(self.I2C_addr, f'{self.W_addr:#04x}', command))
-
-    def clear(self):
-        print("i2cset -y -r 2 {} {} 0x00 0x00 0x50 i".format(self.I2C_addr, f'{self.W_addr:#04x}'))
-        os.system("i2cset -y -r 2 {} {} 0x00 0x00 0x50 i".format(self.I2C_addr, f'{self.W_addr:#04x}'))
-
-    def reset(self):
-        print("i2cset -y -r 2 {} {} 0x00 0x00 0x51 i".format(self.I2C_addr, f'{self.W_addr:#04x}'))
-        os.system("i2cset -y -r 2 {} {} 0x00 0x00 0x51 i".format(self.I2C_addr, f'{self.W_addr:#04x}'))
+        self.send_bytes(data)
