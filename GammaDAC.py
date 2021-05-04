d7ae40bdcbbcdb652ef628ffc222d098e47aadaf
Robert Willian Polli <robert.polli@cnpem.br>
2021-05-04 17:50:30 -0300

    Added readBack Function

--- GammaDAC.py
+++ GammaDAC.py
@@ -1,5 +1,5 @@
-import board
-import busio
+from subprocess import check_output
+import board, busio
 
 class DAC:
 
@@ -13,14 +13,22 @@ class DAC:
         self.I2C_addr = [int(i) for i in self.i2c.scan()][0]
         self.voltageREF = 0
 
-        self.modeRef = {'00': None, '01': 2.5, '10': 2, '11': 4.1}
+        self.modeRef = {'00': None, '01': 2.5, '10': 2.048, '11': 4.1}
 
-    def send_bytes(self, *data):
+    def send_bytes(self, data = '', readBack = False):
         command = []
-        for i in data[0]:
-            command.append(int(i, 2))
 
-        self.i2c.writeto(self.I2C_addr, bytes(command), stop=False)
+        if not(readBack):
+            for i in data:
+                command.append(int(i, 2))
+
+            self.i2c.writeto(self.I2C_addr, bytes(command), stop=False)
+
+        else:
+            a = check_output(["i2cget",  "-y",  "2", f"{self.I2C_addr:#04x}" , f"{self.R_addr:#04x}"])
+            register_Value = int(a.decode(), 16)*16
+
+            return([register_Value, (register_Value*self.voltageREF)/4096])
 
     def power(self, mode, DACs):
         self.send_bytes([f'010000{mode:02b}', f'0000{DACs:04b}', '00000000'])
@@ -51,3 +59,15 @@ class DAC:
             data[0] = f'0011{ch:04b}'
 
         self.send_bytes(data)
+
+    def clear(self):
+        self.send_bytes(['01010000'])
+
+    def reset(self):
+        self.send_bytes(['01010001'])
+
+    def read(self, ch):
+        data = [f'0011{ch:04b}']
+        self.send_bytes(data = data, readBack = False)
+
+        print(self.send_bytes(readBack = True))
