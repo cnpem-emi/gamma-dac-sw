71eef9a579c3c7d8c5e00ff527366a356096af94
Robert Polli <robert.polli@hotmail.com>
2021-05-14 12:39:30 -0300

    Added readback functions

--- GammaDAC.py
+++ GammaDAC.py
@@ -14,20 +14,19 @@ class DAC():
 
         self.modeRef = {'00': None, '01': 2.5, '10': 2.048, '11': 4.1}
 
-    def send_bytes(self, data = '', readBack = False):
+    def send_bytes(self, data='', readBack=False):
         command = []
 
-        if not(readBack):
+        if not (readBack):
             for i in data:
                 command.append(int(i, 2))
 
             self.i2c.writeto(self.I2C_addr, bytes(command), stop=False)
-
         else:
-            a = check_output(["i2cget",  "-y",  "2", f"{self.I2C_addr:#04x}" , f"{self.R_addr:#04x}"])
-            register_Value = int(a.decode(), 16)*16
+            a = check_output(["i2cget", "-y", "2", f"{self.I2C_addr:#04x}", f"{data:#04x}"])
+            register_Value = int(a.decode(), 16) * 16
 
-            return([register_Value, (register_Value*self.voltageREF)/4096])
+            return ([register_Value, (register_Value * self.voltageREF) / 4096])
 
     def power(self, mode, DACs):
         self.send_bytes([f'010000{mode:02b}', f'0000{DACs:04b}', '00000000'])
@@ -39,10 +38,13 @@ class DAC():
         self.voltageREF = self.modeRef[f'{mode:02b}']
         self.send_bytes([f'01110{power:01b}{mode:02b}', '00000000', '00000000'])
 
-    def writeValue(self, value, all_ch = True, ch =0):
+    def writeValue(self, value, all_ch=True, ch=0):
         data = ['10000010']
-        for i in range(len(value[0])):
-            data.append(f'{value[0][i]:08b}')
+        for i in value:
+            data.append(f'{i:08b}')
+        if not (all_ch):
+            data[0] = f'0011{ch:04b}'
+
         self.send_bytes(data)
 
     def writeVolts(self, voltage, all_ch = True, ch =0):
@@ -68,6 +70,5 @@ class DAC():
 
     def read(self, ch):
         data = [f'0011{ch:04b}']
-        self.send_bytes(data = data, readBack = False)
-
-        print(self.send_bytes(readBack = True))
+        print(self.send_bytes(data=int(data[0], 2), readBack=True))
+        #return()
